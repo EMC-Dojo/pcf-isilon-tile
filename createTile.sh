158e88046f09a27b27dd59cf6b6d8a53ca69296f
Thinh Nguyen <thinh.nguyen@emc.com>
2016-10-08 18:28:28 -0400

    Modify the tile and create tile scripts

--- createTile.sh
+++ createTile.sh
@@ -1,47 +1,10 @@
 #!/bin/sh
-RELEASE_TARFILE=dev_releases/*/*.tgz
 
-function copy_release_tar_file {
-  cp $RELEASE_TARFILE $1
-}
-
-function generate_private {
-cat > config/private.yml << EOF
----
-blobstore:
-  s3:
-    access_key_id: ${AWS_ACCESS_KEY}
-    secret_access_key: ${AWS_SECRET_KEY}
-EOF
-}
-
-mkdir releases/build
-pushd releases/build
-  git clone https://github.com/EMC-CMD/libstorage-release
-  pushd libstorage-release
-    generate_private
-    bosh -n create release --force --with-tarball
-    copy_release_tar_file ../../
-  popd
-
-  git clone https://github.com/EMC-CMD/emc-persistence-release
-  pushd emc-persistence-release
-    generate_private
-    git submodule update --init --recursive
-    bosh -n create release --force --with-tarball
-    copy_release_tar_file ../../
-  popd
-
-  git clone https://github.com/cloudfoundry-incubator/routing-release
-  pushd routing-release
-    generate_private
-    ./scripts/update
-    bosh -n create release --force --with-tarball
-    copy_release_tar_file ../../
-  popd
+pushd releases
+wget -O emc-persistence-release-3.tgz https://s3.amazonaws.com/emc-persistence-release/emc-persistence-release-3.tgz
+wget -O libstorage-release-3.tgz https://s3.amazonaws.com/libstorage/libstorage-release-3.tgz
+wget -O cf-routing-release-0.140.0.tgz https://bosh.io/d/github.com/cloudfoundry-incubator/cf-routing-release?v=0.140.0
 popd
-
-rm -rf releases/build
 
 zip -r persistence-storage.pivotal metadata/ releases/
 rm -rf releases/*
